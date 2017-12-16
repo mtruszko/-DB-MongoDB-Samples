7a35a7a7e15e4fe94a3805c48a190ed4380e372d
Unknown <s16391@pja.edu.pl>
2017-12-16 12:02:56 +0100

    [13] by map-reduce

--- zapytanie_13.js
+++ zapytanie_13.js
@@ -1,1 +1,28 @@
-db.people.distinct("job")
+//aggregation
+db.people.distinct("job");
+
+//map-reduce
+var mapFunction = function() {
+    emit(0, { jobs: [this.job]})
+};
+
+var reduceFunction = function(key, value) {
+    var val = []
+    for (var i = 0; i < value.length; i++) {
+        val = val.concat(value[i].jobs)
+    }
+    return { jobs: val};
+};
+
+var finalizeFunction = function(key, value) {
+    let unique = [...new Set(value.jobs)]; 
+    return { jobs: unique}
+}
+        
+db.people.mapReduce(
+        mapFunction,
+        reduceFunction,
+        { out: "13.map_reduce_output",
+          finalize: finalizeFunction 
+         }
+).find();
